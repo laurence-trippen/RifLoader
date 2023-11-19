2951a2545a697fe47cd6e6680366d48d4e5436b1
Laurence Trippen <laurence.trippen@gmail.com>
2023-11-19 15:24:43 +0400

    Add project files.

---
+++ RifLoader/FileChunk.cs
@@ -0,0 +1,89 @@
+﻿using CH.Compressor.Huffman;
+using System.Text;
+
+namespace RifLoader
+{
+    public class FileChunk
+    {
+        public const string COMPRESSED_RIF_IDENTIFIER = "REBCRIF1";
+        public const string UNRECOGNIZED_RIF_IDENTIFIER = "REBINFF2";
+
+        private byte[]? data = null;
+        private bool isCompressed = false;
+
+        public FileChunk(string rifPath)
+        {
+            if (!File.Exists(rifPath))
+                throw new Exception("Couldn't find RIF file: " + rifPath);
+
+            this.data = File.ReadAllBytes(rifPath);
+
+            string rifId = Encoding.ASCII.GetString(GetHeaderIdAsBytes());
+
+            Console.WriteLine("RIF ID: " + rifId);
+
+            this.isCompressed = rifId == COMPRESSED_RIF_IDENTIFIER;
+
+            Console.WriteLine("RIF COMPRESSED: " + this.isCompressed);
+
+            // var compressor = new HuffmanCompressor();
+
+            ExtractHuffmanPackage();
+        }
+
+        private byte[] GetHeaderIdAsBytes()
+        {
+            if (this.data == null)
+            {
+                throw new Exception("Invalid RIF data!");
+            }
+
+            const int headerIdLength = 8;
+
+            byte[] header = new byte[headerIdLength];
+
+            for (int i = 0; i < headerIdLength; i++)
+            {
+                header[i] = this.data[i];
+            }
+
+            return header;
+        }
+
+        private void ExtractHuffmanPackage()
+        {
+            // 8  byte  - char Identifier[8];
+            // 4  byte  - int CompressedDataSize;
+            // 4  byte  - int UncompressedDataSize;
+            // 44 byte  - int CodelengthCount[11];
+            // 256 byte - unsigned char ByteAssignment[256];
+
+            // 316 bytes total
+
+            int compressedDataSize = BitConverter.ToInt32(GetByteSpan(8, 4));
+            int uncompressedDataSize = BitConverter.ToInt32(GetByteSpan(12, 4));
+            byte[] codelengthCount = GetByteSpan(16, 44);
+            byte[] byteAssignment = GetByteSpan(60, 256);
+
+            Console.WriteLine("Compressed data size: " + compressedDataSize);
+            Console.WriteLine("Un Compressed data size: " + uncompressedDataSize);
+        }
+
+        private byte[] GetByteSpan(int offset, int length)
+        {
+            if (this.data == null)
+            {
+                throw new Exception("Invalid RIF data!");
+            }
+
+            byte[] span = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                span[i] = this.data[offset + i];
+            }
+
+            return span;
+        }
+    }
+}
---
+++ RifLoader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace RifLoader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rifLocation = @"C:\\Games\\GOG\\AvP Classic\\avp_rifs";
+            string testRif = "derelict.rif";
+
+            if (!Directory.Exists(rifLocation))
+            {
+                Console.WriteLine("Couldn't find RIF path!");
+                Environment.Exit(1);
+            }
+
+            string targetRif = Path.Combine(rifLocation, testRif);
+            Console.WriteLine(targetRif);
+
+            FileInfo fileInfo = new FileInfo(targetRif);
+            Console.WriteLine(fileInfo.Length);
+
+            FileChunk rifChunk = new FileChunk(targetRif);
+
+            Console.ReadKey();
+        }
+    }
+}
